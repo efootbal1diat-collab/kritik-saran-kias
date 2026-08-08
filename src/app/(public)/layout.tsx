a8a35b24df6774c19375e6bbd4e7b930f89d4be2
11020421 <11020421@KIAS-PC-0353>
2026-08-08 11:25:11 +0700

    Fix layout constraints using Route Group

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+
+
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-full flex flex-col bg-slate-50 text-slate-900">
+        <header className="bg-blue-700 text-white p-4 shadow-md sticky top-0 z-10">
+          <div className="max-w-md mx-auto flex items-center justify-center font-bold text-lg tracking-wide">
+            Kritik & Saran KIAS
+          </div>
+        </header>
+        <main className="max-w-md mx-auto w-full flex-grow p-4 bg-white shadow-sm min-h-[calc(100vh-60px)]">
+          {children}
+        </main>
+      </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,15 +27,8 @@ export default function RootLayout({
       lang="id"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-slate-50 text-slate-900">
-        <header className="bg-blue-700 text-white p-4 shadow-md sticky top-0 z-10">
-          <div className="max-w-md mx-auto flex items-center justify-center font-bold text-lg tracking-wide">
-            Kritik & Saran KIAS
-          </div>
-        </header>
-        <main className="max-w-md mx-auto w-full flex-grow p-4 bg-white shadow-sm min-h-[calc(100vh-60px)]">
-          {children}
-        </main>
+      <body className="min-h-full flex flex-col text-slate-900 bg-slate-50">
+        {children}
       </body>
     </html>
   );
